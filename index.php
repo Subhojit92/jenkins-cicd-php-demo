80d641ad3f3ecf7dab6f296160e424706881cca3
Subhojit92 <135533304+Subhojit92@users.noreply.github.com>
2023-08-01 10:56:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>Index</title>
  </head>
  <body>
- <?php echo '<p>Hello world fro PHP!! Declarative pipeline!!</p>'; ?> 
+ <?php echo '<p>Hello world fro PHP! Declarative pipeline!!</p>'; ?> 
  </body>
 </html>
